f49f6ad0a2526b043e3c78e89354e5f88482486c
Maria Redman <absmcc@gmail.com>
2022-08-08 11:51:18 -0400

    conditional-Rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import Loading from './Loading';
+import Loaded from './Loaded';
 
-function App() {
+
+import './App.css';
+import { findAllByTestId } from '@testing-library/react';
+
+
+function App() { 
+  //const isLoaded = true;
+  const[isLoaded, setIsLoaded] = useState(false)
+  const [username, setUsername] = useState('Maria')
+  const[objectExample, setObjectExample,] = useState({name:"name of Object"})//don't need this
+  const ternary = isLoaded ? "YES" : "NO"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+       <h1>Username is:{username !== "" ? username : "Guest"}</h1>
+       <h1>Username is {username || "Guest"}</h1>
+       <h1>
+        {
+isLoaded && 
+<>
+this is really loaded
+<Loaded/>
+</>
 
-export default App;
+ }</h1>
+       
+       
+       
+       <h1>{String(objectExample.name)}</h1>
+       <h1>{isLoaded ? "YES" : "NO"}</h1>
+
+      {
+      isLoaded ?
+       <Loaded/>
+      :
+     <Loading/>
+    }
+
+
+  <h1>isLoaded = {String(isLoaded)}</h1> 
+    <h1> {ternary} </h1>
+
+    <button onClick={()=>{
+  console.log("button clicked")
+  setIsLoaded(true)
+  setIsLoaded(!isLoaded)
+  }}>Change Loaded</button>
+
+    </div>
+    )
+  }
+  
+  export default App;
+  
